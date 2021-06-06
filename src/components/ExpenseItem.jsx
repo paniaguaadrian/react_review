1e1651ba39331aad04d170406700d503ba0166e9
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2021-06-06 16:47:57 +0200

    1️⃣  V.1.2 🎉 Playing with props, 3rd component, and props tree 🎉

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,17 @@
-const ExpenseItem = () => {
+// Custom Components
+import ExpenseDate from "./ExpenseDate";
+
+// Styles
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
   return (
-    <div>
-      <h2>Expense Item!</h2>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
   );
 };
