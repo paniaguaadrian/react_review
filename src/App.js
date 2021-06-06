af4c6e562f679da7adda16c287c6f72dae4d5586
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2021-06-06 15:44:53 +0200

    1️⃣  V.1.1 🎉 Added first custom component 🎉

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
+// Custom components
+import ExpenseItem from "./components/ExpenseItem";
+
+const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,9 @@
+const ExpenseItem = () => {
+  return (
+    <div>
+      <h2>Expense Item!</h2>
+    </div>
+  );
+};
+
+export default ExpenseItem;
